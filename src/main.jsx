997aadf03b3f7210dfd63e6bb81b925905ac545f
JannatunNimeNishat <nishatnime100@gmail.com>
2023-05-23 21:20:55 +0600

    Menau and cover added parallax effect on cover added

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,15 @@ import './index.css'
 
 import router from './Routes/Routes.jsx'
 import { RouterProvider } from 'react-router-dom'
+//helmet
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className='max-w-screen-xl mx-auto'>
-    <RouterProvider router={router}></RouterProvider>
-    </div>
+    <HelmetProvider>
+      <div className='max-w-screen-xl mx-auto'>
+        <RouterProvider router={router}></RouterProvider>
+      </div>
+    </HelmetProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,30 @@
+
+import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+
+import menuImg from '../../../assets/menu/menu-bg.jpg';
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
+
+const Menu = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>Menu</title>
+            </Helmet>
+           <Cover img={menuImg} 
+           title='Our menu'
+           ></Cover>
+           <PopularMenu></PopularMenu>
+           <Cover img={menuImg} 
+           title='Our menu'
+           ></Cover>
+           <PopularMenu></PopularMenu>
+           <Cover img={menuImg} 
+           title='Our menu'
+           ></Cover>
+           <PopularMenu></PopularMenu>
+        </div>
+    );
+};
+
+export default Menu;
