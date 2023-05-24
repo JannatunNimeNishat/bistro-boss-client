853b60c556b8704aa3a060df83cd181a0ee635c3
JannatunNimeNishat <nishatnime100@gmail.com>
2023-05-24 14:33:27 +0600

    custom hook added for loading menu

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,21 +1,14 @@
 
-import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../hooks/useMenu";
 
 const PopularMenu = () => {
 
-    const [menu, setMenu] = useState([]);
+    //custom hook
+    const [menu] = useMenu()
+    const popular = menu.filter(item => item.category === 'popular');
 
-    useEffect(() => {
-        fetch('menu.json')
-            .then(res => res.json())
-            .then(data => {
-                const popularItems = data?.filter(item => item.category === 'popular')
-                setMenu(popularItems)
-
-            })
-    }, [])
 
 
     return (
@@ -27,13 +20,13 @@ const PopularMenu = () => {
 
             <div className="grid md:grid-cols-2 gap-10">
                 {
-                    menu.map(item => <MenuItem 
+                    popular.map(item => <MenuItem
                         key={item._id}
                         item={item}
                     ></MenuItem>)
                 }
             </div>
-
+            <button className="btn btn-outline border-0 border-b-4 mt-4 ">View Full Menu</button>
         </section>
     );
 };
--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -3,26 +3,39 @@ import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 
 import menuImg from '../../../assets/menu/menu-bg.jpg';
-import PopularMenu from '../../Home/PopularMenu/PopularMenu';
+import useMenu from '../../../hooks/useMenu';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+
+import MenuCategory from '../MenuCategory/MenuCategory';
+
 
 const Menu = () => {
+    const [menu] = useMenu();
+
+    const dessert = menu.filter(item => item.category === 'dessert');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const salad = menu.filter(item => item.category === 'salad');
+    const soup = menu.filter(item => item.category === 'soup');
+    const offered = menu.filter(item => item.category === 'offered');
+
+
     return (
         <div>
             <Helmet>
                 <title>Menu</title>
             </Helmet>
-           <Cover img={menuImg} 
-           title='Our menu'
-           ></Cover>
-           <PopularMenu></PopularMenu>
-           <Cover img={menuImg} 
-           title='Our menu'
-           ></Cover>
-           <PopularMenu></PopularMenu>
-           <Cover img={menuImg} 
-           title='Our menu'
-           ></Cover>
-           <PopularMenu></PopularMenu>
+            <Cover img={menuImg}
+                title='Our menu'
+            ></Cover>
+
+            {/* 1st Today's offer */}
+            <SectionTitle
+                subHeading="Don't miss"
+                heading="TODAY'S OFFER"
+            ></SectionTitle>
+            <MenuCategory items={offered}></MenuCategory>
+
+
         </div>
     );
 };
