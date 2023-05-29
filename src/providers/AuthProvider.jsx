542722ad6372a080102bcc7b134c2b436b48b4c2
JannatunNimeNishat <nishatnime100@gmail.com>
2023-05-29 22:24:29 +0600

    feature - get accesstion on user login register from server and save it to local storage

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState } from "react";
 
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth'
 import app from "../firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext()
 const auth = getAuth(app)
@@ -12,14 +13,14 @@ const AuthProvider = ({ children }) => {
     const [loading, setLoading] = useState(true)
     const googleProvider = new GoogleAuthProvider()
 
-    const createUser = (email,password) =>{
+    const createUser = (email, password) => {
         setLoading(true)
-        return createUserWithEmailAndPassword(auth,email,password);
+        return createUserWithEmailAndPassword(auth, email, password);
     }
 
-    const signIn = (email,password) =>{
+    const signIn = (email, password) => {
         setLoading(true)
-        return signInWithEmailAndPassword(auth,email,password)
+        return signInWithEmailAndPassword(auth, email, password)
     }
     const updateUserProfile = (name, photo) => {
         return updateProfile(auth.currentUser, {
@@ -27,14 +28,14 @@ const AuthProvider = ({ children }) => {
             photoURL: photo
         })
     }
-    
+
     //google signUp
-    const googleSignIn = ()=>{
+    const googleSignIn = () => {
         setLoading(true)
-        return signInWithPopup(auth,googleProvider)
+        return signInWithPopup(auth, googleProvider)
     }
 
-    const logOut = ()=>{
+    const logOut = () => {
         setLoading(true);
         return signOut(auth)
     }
@@ -42,6 +43,31 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setLoading(false);
             setUser(currentUser)
+            //get and set token
+            if (currentUser) {
+                
+                //using axios instead of fetch
+                //no need to use method,headers, body in axios
+                axios.post('http://localhost:5000/jwt',{email: currentUser.email})
+                .then(data =>{
+                    console.log(data.data.token);
+                    localStorage.setItem('access-token',data.data.token)
+                })
+                
+                // const loggedInUser = { email: currentUser.email }
+                /* fetch(`http://localhost:5000/jwt`, {
+                    method: 'GET',
+                    headers: {
+                        'content-type':'application/json'
+                    },
+                    body: JSON.stringify(loggedInUser)
+                })
+                    .then(res => res.json())
+                    .then(data => console.log(data)) */
+            }
+            else{
+                localStorage.removeItem('access-token')
+            }
         })
         return () => {
             unsubscribe();
@@ -57,7 +83,7 @@ const AuthProvider = ({ children }) => {
         logOut,
         updateUserProfile,
         googleSignIn
-    
+
     }
     return (
         <AuthContext.Provider value={authInfo}>
